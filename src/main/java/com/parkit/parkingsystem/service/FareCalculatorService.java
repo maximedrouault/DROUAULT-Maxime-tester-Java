b98e2bffa38bc1b6233498b6c7dc828bb1ad0fa2
maximedrouault <maximedrouault@gmail.com>
2023-12-01 14:24:40 +0100

    Étape 3 : Implémentation de la fonctionnalité de 30 minutes gratuites.

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -13,21 +13,29 @@ public class FareCalculatorService {
         long inHour = ticket.getInTime().getTime();
         long outHour = ticket.getOutTime().getTime();
 
-        //TODO: Some tests are failing here. Need to check if this logic is correct
+        // The timestamps are subtracted and converted from Milliseconds to Hours to deduct the duration of parking in part of hour.
+        // (1000 Milliseconds * 60 seconds * 60 minutes)
+        final int numberOfMillisecondsInAnHour = 1000 * 60 * 60;
+        double duration = (double) (outHour - inHour) / numberOfMillisecondsInAnHour;
 
-        // Subtraction Timestamps and convert from Milliseconds to Hours
-        double duration = (double) (outHour - inHour) / 3600000;
+        // If the duration of parking is less than 30 minutes, the price is 0$ (free).
+        if (duration <= 0.5){
+            ticket.setPrice(0);
+        } else {
 
-        switch (ticket.getParkingSpot().getParkingType()){
-            case CAR: {
-                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
-                break;
+            // Else, the price is calculating with the duration of parking.
+            switch (ticket.getParkingSpot().getParkingType()) {
+                case CAR: {
+                    ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
+                    break;
+                }
+                case BIKE: {
+                    ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+                    break;
+                }
+                default:
+                    throw new IllegalArgumentException("Unkown Parking Type");
             }
-            case BIKE: {
-                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
-                break;
-            }
-            default: throw new IllegalArgumentException("Unkown Parking Type");
         }
     }
 }
